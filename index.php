6420f143d03f6029072255122ff13a8cec6cf332
Ben Keith <ben@benlk.com>
2014-07-24 15:38:13 -0400

    Decruftifying

--- index.php
+++ index.php
@@ -3,70 +3,56 @@
     <head>
         <meta charset="utf-8">
 
-        <title><?php echo $page_title ?></title>
-
-        <?php echo $page_meta ?>
+        <title><?php echo($page_title); ?></title>
+        <!-- Theme for benlk.com -->
+        <?php echo($page_meta); ?>
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="alternate" type="application/rss+xml" title="Subscribe using RSS" href="/rss" />
-        <link rel="alternate" type="application/atom+xml" title="Subscribe using Atom" href="/atom" />
-
-        <link rel="stylesheet" href="<?php echo $template_dir_url ?>custom/style.css">
-        <link rel="stylesheet" href="<?php echo $template_dir_url ?>subdiv.css">
-
+        <!-- Theme Styles -->
+        <link rel="stylesheet" href="<?php echo($template_dir_url); ?>custom/style.css">
+        <link rel="stylesheet" href="<?php echo($template_dir_url); ?>subdiv.css">
         <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
 
-        <link rel="shortcut icon" href="<?php echo $template_dir_url ?>custom/favicon.png">
-
-        <?php echo stripslashes($header_inject) ?>
+	<script src="jquery-1.10.2.min.js"></script>
+        <?php get_header(); ?>
     </head>
 
     <body>
-        <?php echo $content ?>
+        <?php if($is_home) { ?>
+        <header>
+            <div class="row">
+                <div class="one-quarter meta">
+                    <div class="thumbnail">
+                        <img src="<?php echo get_twitter_profile_img($blog_twitter); ?>" alt="profile" />
+                    </div>
 
-        <?=(isset($pagination) && (isset($infinite_scroll) && $infinite_scroll == "off") ? $pagination : "") ?>
+                    <ul>
+                        <li><?php echo($blog_title); ?></li>
+                        <li><a href="mailto:<?php echo($blog_email); ?>?subject=Hello"><?php echo($blog_email); ?></a></li>
+                        <li><a href="http://twitter.com/<?php echo($blog_twitter); ?>">&#64;<?php echo($blog_twitter); ?></a></li>
+                        <li></li>
+                    </ul>
+                </div>
 
-        <?php echo $powered_by ?>
+                <div class="three-quarters post">
+                    <h2><?php echo($intro_title); ?></h2>
 
-        <script src="http://code.jquery.com/jquery-1.9.0.js"></script>
-        <script>
-            var infinite = <?=($infinite_scroll !== "off" && $pagination_on_off !== "off") ? "true;" : "false;";?>
-            var next_page = 2;
-            var loading = false;
-            var no_more_posts = false;
-            $(function() {
-                function load_next_page() {
-                    $.ajax({
-                        url: "?page=" + next_page,
-                        success: function (res) {
-                            next_page++;
-                            var result = $.parseHTML(res);
-                            var articles = $(result).filter(function() {
-                                return $(this).is('article');
-                            });
-                            if (articles.length < 2) {  //There's always one default article, so we should check if  < 2
-                                no_more_posts = true;
-                            }  else {
-                                $('body').append(articles.slice(1));
-                            }
-                            loading = false;
-                        }
-                    });
-                }
+                    <p><?php echo($intro_text); ?></p>
 
-                $(window).scroll(function() {
-                    var when_to_load = $(window).scrollTop() * 0.32;
-                    if (infinite && (loading != true && !no_more_posts) && $(window).scrollTop() + when_to_load > ($(document).height()- $(window).height() ) ) {
-                        // Sometimes the scroll function may be called several times until the loading is set to true.
-                        // So we need to set it as soon as possible
-                        loading = true;
-                        setTimeout(load_next_page,500);
-                    }
-                });
-            });
-        </script>
+                    <?php
+						include "custom/intro-buttons.php"
+						// These are if you want to put some buttons in your header.
+					?>
 
-        <?php echo stripslashes($footer_inject) ?>
+                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                </div>
+            </div>
+        </header>
+        <?php } ?>
+
+        <?php echo($content); ?>
+
+        <?php get_footer(); ?>
     </body>
 </html>
