24b36c31df85f9fc842c22e853ac73fa4bff2f83
Ben Keith <ben@benlk.com>
2015-03-10 13:08:58 -0400

    Adding these because \o/

---
+++ header-category.php
@@ -0,0 +1,13 @@
+<header class="intro">
+    <div class="row">
+        <div class="one-quarter meta">
+            <ul class="actions">
+                <li><a class="button" href="<?php echo($blog_url); ?>">⌫ Home</a></li>
+            </ul>
+        </div>
+
+        <div class="three-quarters post">
+            <h2><?php echo ucwords($category); ?></h2>
+        </div>
+    </div>
+</header>
---
+++ header-single.php
@@ -0,0 +1,14 @@
+<header class="intro">
+    <div class="row">
+        <div class="one-quarter meta">
+            <ul class="actions">
+                <li><a class="button" href="<?php echo($blog_url); ?>">⌫ Home</a></li>
+            </ul>
+        </div>
+
+        <div class="three-quarters post">
+            
+            <h1><?php echo($post_title); ?></h1>
+        </div>
+    </div>
+</header>
