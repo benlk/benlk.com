53166eac8e5be23bff2d6a826a81f31e8fe2ba2f
Ben Keith <ben@benlk.com>
2015-03-12 09:45:48 -0400

    Some single-post cleanup from last night

--- header-single.php
+++ header-single.php
@@ -1,14 +1,6 @@
-<header class="intro">
-    <div class="row">
-        <div class="one-quarter meta">
-            <ul class="actions">
-                <li><a class="button" href="<?php echo($blog_url); ?>">⌫ Home</a></li>
-            </ul>
-        </div>
+<header class="intro single">
 
-        <div class="three-quarters post">
-            
-            <h1><?php echo($post_title); ?></h1>
-        </div>
-    </div>
+	<h1><?php echo($post_title); ?></h1>
+	<a class="" href="<?php echo($blog_url); ?>">⌫ Home</a>
+
 </header>
