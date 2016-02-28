06a8b68c50b216c61927f84d08dd71362d153f2c
Ben Keith <ben@benlk.com>
2016-02-27 21:16:56 -0500

    Clean up index.php, add link to source

--- index.php
+++ index.php
@@ -4,25 +4,32 @@ header("X-Clacks-Overhead: GNU Terry Pratchett");
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo($page_title); ?></title>
 
-        <title><?php echo($page_title); ?></title>
-        <!-- Theme for benlk.com -->
-        <?php echo($page_meta); ?>
+		<!--
 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Theme Styles -->
-        <link rel="stylesheet" href="<?php echo($template_dir_url); ?>css/style.css">
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Ubuntu+Mono|Source+Sans+Pro:400,400italic|Oswald&subset=latin,latin-ext">
+	 _	 _ _____   /
+	 |_____|   |	/
+	 |	 | __|__ .
+
+	Theme source can be found at https://github.com/benlk/benlk.com
+
+-->
+
+		<link rel="stylesheet" href="<?php echo($template_dir_url); ?>css/style.css">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Ubuntu+Mono|Source+Sans+Pro:400,400italic|Oswald&subset=latin,latin-ext">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="icon" type="img/png" href="<?php echo($template_dir_url); ?>custom/favicon.png" />
 
 		<script src="jquery-1.10.2.min.js"></script>
-        <?php get_header(); ?>
-    </head>
+		<?php echo($page_meta); ?>
+		<?php get_header(); ?>
+	</head>
 
-    <body>
+	<body>
 		<?php
 #			var_dump(IS_HOME) . var_dump(isset($category)) . var_dump(IS_SINGLE);
 			if ( IS_HOME ) {
@@ -35,5 +42,5 @@ header("X-Clacks-Overhead: GNU Terry Pratchett");
 			echo($content);
 			get_footer(); 
 		?>
-    </body>
+	</body>
 </html>
