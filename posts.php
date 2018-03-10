fb9a072728842413915ea5035db1a1eaff8bf8db
Ben Keith <ben@benlk.com>
2018-03-10 11:06:13 -0600

    Cleanup of styles for asides.

--- posts.php
+++ posts.php
@@ -3,46 +3,54 @@ $type_array = explode( ' ', $post_status );
 
 $is_aside = in_array( 'aside', $type_array );
 
+/**
+ * Aside layout
+ */
 if ( $is_aside ) {
 ?>
-<article class="<?php echo($post_status); ?>">
-    <div class="row">
-        <div class="one-quarter meta">
+	<article class="<?php echo($post_status); ?>">
+		<div class="row">
+			<div class="one-quarter meta">
+				<ul>
+					<li><?php echo($published_date); ?></li>
+					<li><a href="<?php echo($post_link); ?>">permalink</a></li>
+				</ul>
+			</div>
 
-            <ul>
-                <li><?php echo($published_date); ?></li>
-            </ul>
-        </div>
+			<div class="three-quarters post">
 
-        <div class="three-quarters post">
+				<?php echo($post_content); ?>
 
-            <?php echo($post_content); ?>
+			</div>
+		</div>
+	</article>
 
-        </div>
-    </div>
-</article>
+<?php
+} else {
+/**
+ * Geenral post layout
+ */
+?>
 
-<?php } else { ?>
+	<article class="<?php echo($post_status); ?>">
+		<div class="row">
+			<div class="one-quarter meta">
+				<ul>
+					<li><?php echo($published_date); ?></li>
+					<li><a href="<?php echo($post_link); ?>">permalink</a></li>
+				</ul>
+			</div>
 
-<article class="<?php echo($post_status); ?>">
-    <div class="row">
-        <div class="one-quarter meta">
+			<div class="three-quarters post">
+				<h2><a href="<?php echo($post_link); ?>"><?php echo($post_title); ?></a></h2>
 
-            <ul>
-                <li><?php echo($published_date); ?></li>
-            </ul>
-        </div>
+				<?php echo($post_intro); ?>
 
-        <div class="three-quarters post">
-            <h2><a href="<?php echo($post_link); ?>"><?php echo($post_title); ?></a></h2>
-
-            <?php echo($post_intro); ?>
-
-            <ul class="actions">
-                <li><a class="button" href="<?php echo($post_link); ?>">Continue Reading</a></li>
-            </ul>
-        </div>
-    </div>
-</article>
+				<ul class="actions">
+					<li><a class="button" href="<?php echo($post_link); ?>">Continue Reading</a></li>
+				</ul>
+			</div>
+		</div>
+	</article>
 
 <?php }
